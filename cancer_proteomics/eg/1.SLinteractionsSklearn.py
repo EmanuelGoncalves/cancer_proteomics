SL linear regressions numerical instability bug corrected

cancer_proteomics/eg/1.SLinteractionsSklearn.py
@@ -22,12 +22,15 @@ import logging
 import numpy as np
 import pandas as pd
 import pkg_resources
-from scipy.stats import chi2
-from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import chi2, spearmanr
 from sklearn.linear_model import LinearRegression
 from cancer_proteomics.eg.LMModels import LMModels
-from statsmodels.stats.multitest import multipletests
 from crispy.DataImporter import Proteomics, CRISPR
+from statsmodels.stats.multitest import multipletests
+from sklearn.preprocessing import StandardScaler, quantile_transform
+from crispy.DimensionReduction import dim_reduction_pca
 
 
 LOG = logging.getLogger("Crispy")
@@ -35,18 +38,22 @@ RPATH = pkg_resources.resource_filename("reports", "eg/")
 
 
 class LModel:
-    def __init__(self, Y, X, M):
+    def __init__(
+        self, Y, X, M, normalize=False, fit_intercept=True, copy_X=True, n_jobs=4
+    ):
         self.samples = set.intersection(set(Y.index), set(X.index), set(M.index))
 
-        self.Y = Y.loc[samples]
-        self.Y_ma = np.ma.masked_invalid(self.Y.values)
-
         self.X = X.loc[samples]
+        self.X_ma = np.ma.masked_invalid(self.X.values)
+
+        self.Y = Y.loc[samples]
+
         self.M = M.loc[samples]
 
-        self.normalize = True
-        self.fit_intercept = True
-        self.copy_X = True
+        self.normalize = normalize
+        self.fit_intercept = fit_intercept
+        self.copy_X = copy_X
+        self.n_jobs = n_jobs
 
         self.log = logging.getLogger("Crispy")
 
@@ -55,13 +62,14 @@ class LModel:
             fit_intercept=self.fit_intercept,
             normalize=self.normalize,
             copy_X=self.copy_X,
+            n_jobs=self.n_jobs,
         )
         return regressor
 
     @staticmethod
     def loglike(y_true, y_pred):
         nobs = len(y_true)
-        nobs2 = len(y_true) / 2.0
+        nobs2 = nobs / 2.0
 
         ssr = np.power(y_true - y_pred, 2).sum()
 
@@ -70,92 +78,194 @@ class LModel:
         return llf
 
     def fit_matrix(self):
-        # y_idx, y_var = 1473, "KRT7"
         lms = []
-        for y_idx, y_var in enumerate(self.Y):
-            self.log.info(f"LM={y_var} ({y_idx})")
+
+        # x_idx, x_var, y_var = 510, "CLEC2B", "PSMF1"
+        for x_idx, x_var in enumerate(self.X):
+            self.log.info(f"LM={x_var} ({x_idx})")
 
             # Mask NaNs
-            y_ma = np.ma.mask_rowcols(self.Y_ma[:, [y_idx]], axis=0)
+            x_ma = np.ma.mask_rowcols(self.X_ma[:, [x_idx]], axis=0)
 
             # Build matrices
-            y = self.Y.iloc[~y_ma.mask.any(axis=1), y_idx]
-            y = pd.Series(StandardScaler().fit_transform(y.to_frame())[:, 0], index=y.index)
+            x = self.X.iloc[~x_ma.mask.any(axis=1), x_idx]
+            x = pd.DataFrame(
+                StandardScaler().fit_transform(x.to_frame()),
+                index=x.index,
+                columns=[x_var],
+            )
 
-            x = self.X.iloc[~y_ma.mask.any(axis=1), :]
-            x = pd.DataFrame(StandardScaler().fit_transform(x), index=x.index, columns=x.columns)
+            y = self.Y.iloc[~x_ma.mask.any(axis=1), :]
+            y = pd.DataFrame(
+                StandardScaler().fit_transform(y), index=y.index, columns=y.columns
+            )
 
-            m = self.M.iloc[~y_ma.mask.any(axis=1), :]
+            m = self.M.iloc[~x_ma.mask.any(axis=1), :]
             m = m.loc[:, m.std() > 0]
+            m += np.random.normal(0, 1e-4, m.shape)
+            # m = m.loc[:, (m.dtypes != np.uint8) | (m.sum() > 3)]
 
             # Fit covariate model
             lm_small = self.model_regressor().fit(m, y)
             lm_small_ll = self.loglike(y, lm_small.predict(m))
 
-            # Iterate over all possible features
-            y_lms = []
-            # x_idx, x_var = 3309, "CD8B"
-            for x_idx, x_var in enumerate(self.X):
-                x_ = np.concatenate([m, x.iloc[:, [x_idx]]], axis=1)
+            # Fit full model: covariates + feature
+            lm_full_x = np.concatenate([m, x], axis=1)
+            lm_full = self.model_regressor().fit(lm_full_x, y)
+            lm_full_ll = self.loglike(y, lm_full.predict(lm_full_x))
 
-                # Fit full model
-                lm_full = self.model_regressor().fit(x_, y)
-                lm_full_ll = self.loglike(y, lm_full.predict(x_))
+            # Log-ratio test
+            lr = 2 * (lm_full_ll - lm_small_ll)
+            lr_pval = chi2(1).sf(lr)
 
-                # Log-ratio test
-                lr = 2 * (lm_full_ll - lm_small_ll)
-                lr_pval = chi2(1).sf(lr)
-
-                # Results
-                y_lms.append(
-                    dict(
-                        y=y_var,
-                        x=x_var,
-                        n=y.shape[0],
-                        b=lm_full.coef_[-1],
-                        lr=lr,
-                        pval=lr_pval,
-                        covs=m.shape[1],
-                    )
+            # Assemble + append results
+            res = pd.DataFrame(
+                dict(
+                    y=y.columns,
+                    x=x_var,
+                    n=len(x),
+                    b=lm_full.coef_[:, -1],
+                    lr=lr.values,
+                    covs=m.shape[1],
+                    pval=lr_pval,
+                    fdr=multipletests(lr_pval, method="fdr_bh")[1],
                 )
+            )
+            lms.append(res)
 
-            y_lms = pd.DataFrame(y_lms)
-            y_lms["fdr"] = multipletests(y_lms["pval"], method="fdr_bh")[1]
-
-            lms.append(y_lms)
         lms = pd.concat(lms, ignore_index=True).sort_values("pval")
+
         return lms
 
 
 # Data-sets
 #
+
 prot_obj, crispr_obj = Proteomics(), CRISPR()
+
 
 # Samples
 #
+
 samples = set.intersection(
-    set(prot_obj.get_data()),
-    set(crispr_obj.get_data(dtype="merged")),
+    set(prot_obj.get_data()), set(crispr_obj.get_data(dtype="merged"))
 )
 LOG.info(f"Samples: {len(samples)}")
 
+
 # Filter data-sets
-#
+
 prot = prot_obj.filter(subset=samples, perc_measures=0.03)
 LOG.info(f"Proteomics: {prot.shape}")
 
 crispr = crispr_obj.filter(dtype="merged", subset=samples, abs_thres=0.5, min_events=5)
 LOG.info(f"CRISPR: {crispr.shape}")
 
+
+#
+#
+
+crispr_all = crispr_obj.filter(dtype="merged", subset=samples)
+crispr_pca = dim_reduction_pca(crispr_all)
+
+prot_all = prot_obj.filter(subset=samples, perc_measures=0.5)
+prot_all = prot_all.T.fillna(prot_all.T.mean()).T
+prot_pca = dim_reduction_pca(prot_all)
+
+s_pg_corr = pd.read_csv(
+    f"{RPATH}/2.SLProteinInteractions_gexp_prot_samples_corr.csv", index_col=0
+)
+
+putative_covariates = pd.concat(
+    [
+        prot.count().rename("Proteomics n. measurements"),
+        prot_obj.protein_raw.median().rename("Global proteomics"),
+        s_pg_corr["corr"].rename("gexp_prot_corr"),
+        pd.get_dummies(prot_obj.ss["msi_status"]),
+        pd.get_dummies(prot_obj.ss["growth_properties"]),
+        pd.get_dummies(prot_obj.ss["tissue"])["Haematopoietic and Lymphoid"],
+        prot_obj.ss.reindex(
+            index=samples, columns=["ploidy", "mutational_burden", "growth"]
+        ),
+    ],
+    axis=1,
+).loc[crispr_pca[0].index]
+
+putative_covariates_corr = {
+    d: pd.DataFrame(
+        {
+            f: {
+                c: spearmanr(df[f], putative_covariates[c], nan_policy="omit")[0]
+                for c in putative_covariates
+            }
+            for f in df
+        }
+    )
+    for d, df in [("crispr", crispr_pca[0]), ("prot", prot_pca[0])]
+}
+
+for d, df in putative_covariates_corr.items():
+    fig = sns.clustermap(
+        df,
+        cmap="Spectral",
+        center=0,
+        cbar=False,
+        annot=True,
+        fmt=".2f",
+        linewidths=0.5,
+        annot_kws={"fontsize": 5},
+        cbar_pos=None,
+        figsize=[df.shape[0] * 0.4] * 2,
+    )
+    fig.ax_heatmap.set_xlabel("")
+    fig.ax_heatmap.set_ylabel("")
+
+    plt.savefig(
+        f"{RPATH}/1.SL_covariates_pca_corrmap_{d}.pdf",
+        bbox_inches="tight",
+        transparent=True,
+    )
+    plt.close("all")
+
+
 # Covariates
 #
+
 covariates = LMModels.define_covariates(
-    institute=crispr_obj.merged_institute, cancertype=False, tissuetype=True, mburden=False, ploidy=False
-)
+        institute=crispr_obj.merged_institute,
+        medium=True,
+        cancertype=False,
+        tissuetype=True,
+        mburden=False,
+        ploidy=True,
+    )
+# covariates = pd.concat([
+#     covariates,
+#     pd.get_dummies(prot_obj.ss["tissue"])["Haematopoietic and Lymphoid"],
+# ], axis=1)
+covariates = covariates.reindex(samples).dropna()
+
+samples = set(covariates.index)
+LOG.info(f"Samples={len(samples)}; Covariates={covariates.shape[1]}")
+
+
+# Reduce to independent variables with more observations than covariates
+#
+
+prot = prot[prot[samples].count(1) > (covariates.shape[1] + 1)]
+
 
 # Protein ~ CRISPR LMMs
 #
+
 prot_lm = LModel(
-    Y=prot[samples].T, X=crispr[samples].T, M=covariates.loc[samples]
+    Y=crispr[samples].T, X=prot[samples].T, M=covariates.loc[samples]
 ).fit_matrix()
-prot_lm.to_csv(f"{RPATH}/lm_sklearn_protein_crispr.csv.gz", index=False, compression="gzip")
+
+
+# Export
+#
+
+prot_lm.to_csv(
+    f"{RPATH}/lm_sklearn_protein_crispr.csv.gz", index=False, compression="gzip"
+)

cancer_proteomics/eg/2.SLinteractionsAnalysis.py
@@ -97,27 +97,26 @@ patt_high = list(patt.query("cluster == 'High'")["gene"])
 #
 
 sl_lm = pd.read_csv(f"{RPATH}/lm_sklearn_protein_crispr.csv.gz").dropna()
-gi_list = sl_lm.query("fdr < .1").query("n > 50")
 
 # CORUM
-gi_list["corum"] = [
-    int((p1, p2) in corum_db.db_melt_symbol) for p1, p2 in gi_list[["y", "x"]].values
+sl_lm["corum"] = [
+    int((p1, p2) in corum_db.db_melt_symbol) for p1, p2 in sl_lm[["y", "x"]].values
 ]
 
 # BioGRID
-gi_list["biogrid"] = [
-    int((p1, p2) in biogrid_db.biogrid) for p1, p2 in gi_list[["y", "x"]].values
+sl_lm["biogrid"] = [
+    int((p1, p2) in biogrid_db.biogrid) for p1, p2 in sl_lm[["y", "x"]].values
 ]
 
 # HuRI
-gi_list["huri"] = [
-    int((p1, p2) in huri_db.huri) for p1, p2 in gi_list[["y", "x"]].values
+sl_lm["huri"] = [
+    int((p1, p2) in huri_db.huri) for p1, p2 in sl_lm[["y", "x"]].values
 ]
 
 # String distance
 ppi = PPI().build_string_ppi(score_thres=900)
-gi_list = PPI.ppi_annotation(gi_list, ppi, x_var="x", y_var="y", ppi_var="string_dist")
-gi_list = gi_list.assign(string=(gi_list["string_dist"] == "1").astype(int))
+sl_lm = PPI.ppi_annotation(sl_lm, ppi, x_var="x", y_var="y", ppi_var="string_dist")
+sl_lm = sl_lm.assign(string=(sl_lm["string_dist"] == "1").astype(int))
 
 # Previously reported SL
 known_sl = pd.read_excel(f"{DPATH}/sang_lee_nature_comms_sl_network.xlsx")
@@ -128,16 +127,85 @@ known_sl = {
     for p1, p2 in [(p[0], p[1]), (p[1], p[0])]
     if p1 != p2
 }
-gi_list["sl"] = [
-    int((p1, p2) in known_sl) for p1, p2 in gi_list[["y", "x"]].values
-]
+sl_lm["sl"] = [int((p1, p2) in known_sl) for p1, p2 in sl_lm[["y", "x"]].values]
 
 # Attenuated protein
-gi_list["attenuated"] = gi_list["y"].isin(patt_high).astype(int)
+sl_lm["attenuated"] = sl_lm["x"].isin(patt_high).astype(int)
 
 # Sort by LR p-value
-gi_list = gi_list.sort_values("pval")
+sl_lm = sl_lm.sort_values("pval")
+
+# Top significant associations
+gi_list = sl_lm.query("fdr < .1")
 print(gi_list.head(60))
+
+
+# Enrichment recall curves
+#
+
+dbs = ["corum", "biogrid", "string", "huri", "sl", "attenuated"]
+dbs_pal = dict(
+    biogrid=sns.color_palette("tab20c").as_hex()[0],
+    corum=sns.color_palette("tab20c").as_hex()[4],
+    string=sns.color_palette("tab20c").as_hex()[8],
+    huri=sns.color_palette("tab20c").as_hex()[12],
+    attenuated=sns.color_palette("tab20b").as_hex()[8],
+    sl=sns.color_palette("tab20b").as_hex()[4],
+)
+
+dbs_rc = dict()
+for db in dbs:
+    db_df = gi_list.reset_index(drop=True)[db]
+
+    rc_df_y = np.cumsum(db_df) / np.sum(db_df)
+    rc_df_x = np.array(db_df.index) / db_df.shape[0]
+    rc_df_auc = auc(rc_df_x, rc_df_y)
+
+    dbs_rc[db] = dict(x=list(rc_df_x), y=list(rc_df_y), auc=rc_df_auc)
+
+# Recall curves
+_, ax = plt.subplots(1, 1, figsize=(3, 3), dpi=600)
+
+for db in dbs_rc:
+    ax.plot(
+        dbs_rc[db]["x"],
+        dbs_rc[db]["y"],
+        label=f"{db} (AUC={dbs_rc[db]['auc']:.2f})",
+        c=dbs_pal[db],
+    )
+
+ax.plot([0, 1], [0, 1], "k--", lw=0.3)
+ax.legend(loc="lower right", frameon=False)
+
+ax.set_ylabel("Cumulative sum")
+ax.set_xlabel("Ranked correlation")
+ax.grid(True, axis="both", ls="-", lw=0.1, alpha=1.0, zorder=0)
+
+plt.savefig(f"{RPATH}/2.SL_roc_curves.pdf", bbox_inches="tight", transparent=True)
+plt.close("all")
+
+# Barplot
+plot_df = pd.DataFrame([dict(ppi=db, auc=dbs_rc[db]["auc"]) for db in dbs_rc])
+
+_, ax = plt.subplots(1, 1, figsize=(3, 1.5), dpi=600)
+
+sns.barplot(
+    "auc",
+    "ppi",
+    data=plot_df,
+    orient="h",
+    linewidth=0.0,
+    saturation=1.0,
+    palette=dbs_pal,
+    ax=ax,
+)
+
+ax.set_ylabel("")
+ax.set_xlabel("Recall curve AUC")
+ax.grid(True, ls="-", lw=0.1, alpha=1.0, zorder=0, axis="x")
+
+plt.savefig(f"{RPATH}/2.SL_roc_barplot.pdf", bbox_inches="tight", transparent=True)
+plt.close("all")
 
 
 # Volcano
@@ -145,17 +213,16 @@ print(gi_list.head(60))
 
 s_transform = MinMaxScaler(feature_range=[1, 10])
 
-_, ax = plt.subplots(1, 1, figsize=(3, 1.5), dpi=600)
+_, ax = plt.subplots(1, 1, figsize=(4.5, 2.5), dpi=600)
 
 for t, df in gi_list.groupby("string_dist"):
     sc = ax.scatter(
         -np.log10(df["pval"]),
         df["b"],
-        s=s_transform.fit_transform(df[["b"]].abs()),
+        s=s_transform.fit_transform(df[["n"]]),
         color=GIPlot.PPI_PAL[t],
         label=t,
-        edgecolor="white",
-        lw=0.1,
+        lw=0.0,
         alpha=0.5,
     )
 
@@ -165,15 +232,14 @@ legend1 = ax.legend(
     frameon=False,
     prop={"size": 4},
     title="PPI distance",
-    loc="center left",
-    bbox_to_anchor=(1, 0.5),
+    loc="lower right",
 )
-legend1.get_title().set_fontsize("2")
+legend1.get_title().set_fontsize("4")
 ax.add_artist(legend1)
 
 handles, labels = sc.legend_elements(
     prop="sizes",
-    num=4,
+    num=8,
     func=lambda x: s_transform.inverse_transform(np.array(x).reshape(-1, 1))[:, 0],
 )
 legend2 = (
@@ -181,22 +247,19 @@ legend2 = (
         handles,
         labels,
         loc="upper right",
-        title="Effect size (abs)",
+        title="# samples",
         frameon=False,
-        prop={"size": 2},
+        prop={"size": 4},
     )
-        .get_title()
-        .set_fontsize("2")
+    .get_title()
+    .set_fontsize("4")
 )
 
-plt.ylabel("Effect size (beta)")
-plt.xlabel("Association p-value (-log10)")
+ax.set_ylabel("Effect size (beta)")
+ax.set_xlabel("Association p-value (-log10)")
+ax.set_title("CRISPR ~ Protein associations")
 
-plt.savefig(
-    f"{RPATH}/2.SL_volcano.png",
-    transparent=True,
-    bbox_inches="tight",
-)
+plt.savefig(f"{RPATH}/2.SL_volcano.png", transparent=True, bbox_inches="tight")
 plt.close("all")
 
 
@@ -206,17 +269,23 @@ plt.close("all")
 df_corr = pd.read_csv(f"{RPATH}/2.SLProteinInteractions.csv.gz")
 novel_ppis = df_corr.query(f"(prot_fdr < .05) & (prot_corr > 0.5)")
 
-plot_df = pd.concat([
-    pd.Series(list(novel_ppis["protein1"]) + list(novel_ppis["protein2"])).value_counts().rename("ppis"),
-    gi_list[gi_list["b"].abs() > .5]["y"].value_counts().rename("gis"),
-], axis=1).dropna()
+plot_df = pd.Series(
+    list(novel_ppis["protein1"]) + list(novel_ppis["protein2"])
+).value_counts()
+plot_df = pd.concat(
+    [
+        plot_df.rename("ppis"),
+        gi_list["x"].value_counts().rename("gis"),
+    ],
+    axis=1,
+).dropna()
 
 _, ax = plt.subplots(1, 1, figsize=(2, 2), dpi=600)
 
 ax.scatter(plot_df["ppis"], plot_df["gis"], c=GIPlot.PAL_DBGD[2], s=5, linewidths=0)
 
-ax.set_yscale('log')
-ax.set_xscale('log')
+ax.set_yscale("log")
+ax.set_xscale("log")
 
 ax.set_xlabel("Number of PPIs")
 ax.set_ylabel(f"Number of GIs")
@@ -225,19 +294,9 @@ ax.grid(True, ls=":", lw=0.1, alpha=1.0, zorder=0, axis="both")
 
 cor, pval = spearmanr(plot_df["ppis"], plot_df["gis"])
 annot_text = f"Spearman's R={cor:.2g}, p-value={pval:.1e}"
-ax.text(
-    0.95,
-    0.05,
-    annot_text,
-    fontsize=4,
-    transform=ax.transAxes,
-    ha="right",
-)
+ax.text(0.95, 0.05, annot_text, fontsize=4, transform=ax.transAxes, ha="right")
 
-plt.savefig(
-    f"{RPATH}/2.SL_ppis_gis_correlation.pdf",
-    bbox_inches="tight",
-)
+plt.savefig(f"{RPATH}/2.SL_ppis_gis_correlation.pdf", bbox_inches="tight")
 plt.close("all")
 
 
@@ -253,7 +312,7 @@ gi_pairs = [
     ("PRKAR1A", "PRKAR1A"),
 ]
 
-# p, c = ("PCYT1A", "GUK1")
+# p, c = ("VIM", "FERMT2")
 for p, c in gi_pairs:
     plot_df = pd.concat(
         [
@@ -276,7 +335,9 @@ for p, c in gi_pairs:
     )
     plt.close("all")
 
-    ax = GIPlot.gi_tissue_plot(f"{p}_gexp", f"{c}_crispr", plot_df.dropna(subset=[f"{c}_crispr", f"{p}_gexp"]))
+    ax = GIPlot.gi_tissue_plot(
+        f"{p}_gexp", f"{c}_crispr", plot_df.dropna(subset=[f"{c}_crispr", f"{p}_gexp"])
+    )
     ax.set_xlabel(f"{p}\nGene expression (RNA-Seq voom)")
     ax.set_ylabel(f"{c}\nCRISPR log FC")
     plt.savefig(
@@ -285,4 +346,3 @@ for p, c in gi_pairs:
         bbox_inches="tight",
     )
     plt.close("all")
-

cancer_proteomics/eg/LMModels.py
@@ -351,7 +351,7 @@ class LMModels:
         if std_filter:
             covariates = covariates.loc[:, covariates.std() > 0]
 
-        return covariates.dropna()
+        return covariates.dropna().astype(np.float)
 
     @staticmethod
     def kinship(k, decimal_places=5):
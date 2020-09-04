a5eef4cd04f96fe59d2f3ce8d1d9ce7deff26201
eg14 <emanuelvgoncalves@gmail.com>
2020-09-04 14:47:05 +0100

    Multiomics analysis performed with covariates

---
+++ cancer_proteomics/notebooks/Associations.py
@@ -0,0 +1,194 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.3'
+#       jupytext_version: 1.0.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %matplotlib inline
+# %autosave 0
+# %load_ext autoreload
+# %autoreload 2
+
+import logging
+import pandas as pd
+import pkg_resources
+from scipy.stats import skew
+from crispy.DataImporter import PPI
+from sklearn.decomposition import PCA
+from crispy.LMModels import LMModels, LModel
+from cancer_proteomics.notebooks import DataImport
+
+
+LOG = logging.getLogger("cancer_proteomics")
+DPATH = pkg_resources.resource_filename("data", "/")
+PPIPATH = pkg_resources.resource_filename("data", "ppi/")
+TPATH = pkg_resources.resource_filename("tables", "/")
+RPATH = pkg_resources.resource_filename("cancer_proteomics", "plots/")
+
+
+# ### Imports
+
+# PPI
+ppi = PPI(ddir=PPIPATH).build_string_ppi(score_thres=900)
+
+# Read samplesheet
+ss = DataImport.read_samplesheet()
+
+# Read proteomics (Proteins x Cell lines)
+prot = DataImport.read_protein_matrix(map_protein=True, min_measurements=300)
+
+# Read Transcriptomics
+gexp = DataImport.read_gene_matrix()
+
+# Read CRISPR
+crispr = DataImport.read_crispr_matrix()
+crispr_institute = DataImport.read_crispr_institute()
+crispr_skew = crispr.apply(skew, axis=1, nan_policy="omit").astype(float)
+
+# Read Drug-response
+drespo = DataImport.read_drug_response(min_measurements=300)
+dtargets = DataImport.read_drug_target()
+drespo_skew = drespo.apply(skew, axis=1, nan_policy="omit").astype(float)
+
+
+# ### Gene expression dimension reduction
+
+gexp_pca = pd.DataFrame(
+    PCA(n_components=10).fit_transform(gexp.T), index=gexp.columns
+).add_prefix("PC")
+
+
+# ### Linear regressions
+
+X = LMModels.transform_matrix(prot, t_type="None", fillna_func=None).T
+M2 = LMModels.transform_matrix(gexp, t_type="None").T
+features = set.intersection(set(X), set(M2))
+
+# ## Drug response
+Y = LMModels.transform_matrix(drespo, t_type="None").T
+
+# Covariates with and without gene expression (transcript and PCA) as covariates
+covs_drug = (
+    pd.concat(
+        [
+            ss["media"].str.get_dummies(),
+            drespo.mean().rename("MeanIC50"),
+            ss["growth_properties"].str.get_dummies(),
+            ss[["replicates_correlation", "growth", "ploidy"]],
+            ss["tissue"].str.get_dummies()["Haematopoietic and Lymphoid"],
+        ],
+        axis=1,
+    )
+    .reindex(index=prot.columns)
+    .dropna()
+)
+
+M_without_gexp = pd.concat([covs_drug], axis=1).dropna()
+M_with_gexp = pd.concat([covs_drug, gexp_pca], axis=1).dropna()
+
+# Overlapping samples
+samples = set.intersection(
+    set(Y.index), set(X.index), set(M_with_gexp.index), set(M2.index)
+)
+
+# Associations without gene expression as covaraites
+lm_prot_drug = LModel(
+    Y=Y.loc[samples], X=X.loc[samples, features], M=M_without_gexp.loc[samples]
+).fit_matrix()
+lm_prot_drug = LMModels.multipletests(lm_prot_drug).sort_values("fdr")
+
+# Associations with gene expression as covariate
+lm_prot_drug_gexp = LModel(
+    Y=Y.loc[samples],
+    X=X.loc[samples, features],
+    M=M_with_gexp.loc[samples],
+    M2=M2.loc[samples, features],
+).fit_matrix()
+lm_prot_drug_gexp = LMModels.multipletests(lm_prot_drug_gexp).sort_values("fdr")
+
+# Merge associations
+lm_drug = pd.concat(
+    [
+        lm_prot_drug_gexp.set_index(["y_id", "x_id"]),
+        lm_prot_drug.set_index(["y_id", "x_id"])[
+            ["beta", "lr", "pval", "fdr"]
+        ].add_prefix("nc_"),
+    ],
+    axis=1,
+).reset_index()
+
+# Annotate merged table
+lm_drug = DataImport.lm_ppi_annotate_table(
+    lm_drug, ppi, drespo_skew, drug_targets=dtargets
+)
+lm_drug = lm_drug.sort_values("fdr")
+lm_drug.to_csv(
+    f"{TPATH}/lm_sklearn_degr_drug_annotated.csv.gz", compression="gzip", index=False
+)
+
+# ## CRISPR
+Y = LMModels.transform_matrix(crispr, t_type="None").T
+
+# Covariates with and without gene expression (transcript and PCA) as covariates
+covs_crispr = (
+    pd.concat(
+        [
+            ss["media"].str.get_dummies(),
+            crispr_institute.str.get_dummies(),
+            ss["growth_properties"].str.get_dummies(),
+            ss[["replicates_correlation", "growth", "ploidy"]],
+            ss["tissue"].str.get_dummies()["Haematopoietic and Lymphoid"],
+        ],
+        axis=1,
+    )
+    .reindex(index=prot.columns)
+    .dropna()
+)
+
+M_without_gexp = pd.concat([covs_crispr], axis=1).dropna()
+M_with_gexp = pd.concat([covs_crispr, gexp_pca], axis=1).dropna()
+
+# Overlapping samples
+samples = set.intersection(
+    set(Y.index), set(X.index), set(M_with_gexp.index), set(M2.index)
+)
+
+# Associations without gene expression
+lm_prot_crispr = LModel(
+    Y=Y.loc[samples], X=X.loc[samples, features], M=M_without_gexp.loc[samples]
+).fit_matrix()
+
+# Associations with gene expression as covariate
+lm_prot_crispr_gexp = LModel(
+    Y=Y.loc[samples],
+    X=X.loc[samples, features],
+    M=M_with_gexp.loc[samples],
+    M2=M2.loc[samples, features],
+).fit_matrix()
+
+# Merge associations
+lm_crispr = pd.concat(
+    [
+        lm_prot_crispr_gexp.set_index(["y_id", "x_id"]),
+        lm_prot_crispr.set_index(["y_id", "x_id"])[
+            ["beta", "lr", "pval", "fdr"]
+        ].add_prefix("nc_"),
+    ],
+    axis=1,
+).reset_index()
+
+# Annotate merged table
+lm_crispr = DataImport.lm_ppi_annotate_table(lm_crispr, ppi, crispr_skew)
+lm_crispr = lm_crispr.sort_values("fdr")
+lm_crispr.to_csv(
+    f"{TPATH}/lm_sklearn_degr_crispr_annotated.csv.gz", compression="gzip", index=False
+)
--- cancer_proteomics/notebooks/MultiOmicsIntegration.py
+++ cancer_proteomics/notebooks/MultiOmicsIntegration.py
@@ -18,33 +18,13 @@
 # %load_ext autoreload
 # %autoreload 2
 
-import gseapy
 import logging
-import numpy as np
 import pandas as pd
 import pkg_resources
-import seaborn as sns
-import numpy.ma as ma
-import itertools as it
 import matplotlib.pyplot as plt
-from natsort import natsorted
 from crispy.GIPlot import GIPlot
-from adjustText import adjust_text
 from crispy.MOFA import MOFA, MOFAPlot
-from sklearn.metrics.ranking import auc
-from crispy.Enrichment import Enrichment
-from crispy.CrispyPlot import CrispyPlot
-from scipy.stats import pearsonr, spearmanr, ttest_ind
-from sklearn.mixture import GaussianMixture
-from statsmodels.stats.multitest import multipletests
-from mpl_toolkits.axes_grid1.inset_locator import inset_axes
-from crispy.DataImporter import CORUM, BioGRID, PPI, HuRI
-from cancer_proteomics.notebooks import (
-    DataImport,
-    two_vars_correlation,
-    PALETTE_TTYPE,
-    PALETTE_PERTURB,
-)
+from cancer_proteomics.notebooks import DataImport, two_vars_correlation, PALETTE_TTYPE
 
 
 LOG = logging.getLogger("cancer_proteomics")
@@ -60,7 +40,7 @@ RPATH = pkg_resources.resource_filename("cancer_proteomics", "plots/")
 ss = DataImport.read_samplesheet()
 
 # Read proteomics (Proteins x Cell lines)
-prot = DataImport.read_protein_matrix(map_protein=True)
+prot = DataImport.read_protein_matrix(map_protein=True, min_measurements=300)
 
 # Read proteomics BROAD (Proteins x Cell lines)
 prot_broad = DataImport.read_protein_matrix_broad()
@@ -75,12 +55,7 @@ crispr = DataImport.read_crispr_matrix()
 methy = DataImport.read_methylation_matrix()
 
 # Read Drug-response
-drespo = DataImport.read_drug_response()
-drespo = drespo.set_index(pd.Series([";".join(map(str, i)) for i in drespo.index]))
-
-dmaxc = DataImport.read_drug_max_concentration()
-dmaxc.index = [";".join(map(str, i)) for i in dmaxc.index]
-dmaxc = dmaxc.reindex(drespo.index)
+drespo = DataImport.read_drug_response(min_measurements=300)
 
 
 # ### Covariates
@@ -100,24 +75,33 @@ covariates = pd.concat(
         pd.get_dummies(ss["tissue"])[["Haematopoietic and Lymphoid", "Lung"]],
         ss[["ploidy", "mutational_burden", "growth", "size"]],
         ss["replicates_correlation"].rename("RepsCorrelation"),
+        prot.mean().rename("MeanProteomics"),
+        methy.mean().rename("MeanMethylation"),
+        drespo.mean().rename("MeanDrugResponse"),
     ],
     axis=1,
 )
 
 # ### MOFA
-#
+
+# Group Haematopoietic and Lymphoid cell lines separetly from the rest
 groupby = ss.loc[prot.columns, "tissue"].apply(
     lambda v: "Haem" if v == "Haematopoietic and Lymphoid" else "Other"
 )
 
 mofa = MOFA(
     views=dict(proteomics=prot, transcriptomics=gexp, methylation=methy, drespo=drespo),
+    covariates=dict(
+        proteomics=covariates[["MeanProteomics"]].dropna(),
+        methylation=covariates[["MeanMethylation"]].dropna(),
+        drespo=covariates[["MeanDrugResponse"]].dropna(),
+    ),
     groupby=groupby,
     iterations=2000,
     use_overlap=False,
     convergence_mode="slow",
     factors_n=15,
-    from_file=f"{TPATH}/MultiOmics.hdf5",
+    from_file=f"{TPATH}/MultiOmics_covs.hdf5",
     verbose=2,
 )
 
@@ -130,9 +114,9 @@ for f in mofa.factors:
 
     for c in covariates:
         fc_samples = list(covariates.reindex(mofa.factors[f].index)[c].dropna().index)
-        n_factors_corr[f][c] = pearsonr(
+        n_factors_corr[f][c] = two_vars_correlation(
             mofa.factors[f][fc_samples], covariates[c][fc_samples]
-        )[0]
+        )["corr"]
 n_factors_corr = pd.DataFrame(n_factors_corr)
 
 # Factor clustermap
@@ -165,7 +149,7 @@ plt.close("all")
 
 
 # ### MOFA Factor 1 and 2
-f_x, f_y = "F1", "F3"
+f_x, f_y = "F1", "F2"
 
 plot_df = pd.concat(
     [
@@ -202,218 +186,3 @@ for z in ["VIM_proteomics", "CDH1_proteomics"]:
         dpi=600,
     )
     plt.close("all")
-
-
-# ### Perturbation proteomics
-
-manifest = DataImport.read_protein_perturbation_manifest()
-manifest = manifest[~manifest["External Patient ID"].isin(["MDA-MB-468", "MRC-5"])]
-manifest = manifest[
-    ~((manifest["Cell Line"] == "BT-549 1% FBS") & (manifest["Date on sample"] == "4/7/19 "))
-]
-manifest = manifest.drop(
-    ["200627_b2-1-t5-1_00wuz_00yid_m03_s_1", "200623_b2-1-t4-1_00wuz_00yh3_m01_s_1"]
-)
-manifest = manifest[["0.5%FBS" not in v for v in manifest["Cell Line"]]]
-
-prot_perturb = DataImport.read_protein_perturbation(map_protein=True)
-prot_perturb = prot_perturb[manifest.index]
-
-prot_perturb_reps_corr = pd.DataFrame(
-    [
-        {
-            **two_vars_correlation(prot_perturb[c1], prot_perturb[c2]),
-            **dict(
-                sample1=c1,
-                sample2=c2,
-                cellline1=manifest.loc[c1, "Cell Line"],
-                cellline2=manifest.loc[c2, "Cell Line"],
-            ),
-        }
-        for c1, c2 in it.combinations(list(prot_perturb), 2)
-    ]
-)
-
-#
-plot_df = prot_perturb_reps_corr.query("cellline1 == cellline2")
-plot_df.cellline1 = plot_df.cellline1.astype("category")
-plot_df.cellline1.cat.set_categories(PALETTE_PERTURB.keys(), inplace=True)
-plot_df = plot_df.sort_values("cellline1")
-
-fig, ax = plt.subplots(1, 1, figsize=(2, 2), dpi=600)
-
-sns.boxplot(
-    "corr",
-    "cellline1",
-    data=plot_df,
-    orient="h",
-    saturation=1,
-    palette=PALETTE_PERTURB,
-    showcaps=False,
-    boxprops=dict(linewidth=0.3),
-    whiskerprops=dict(linewidth=0.3),
-    flierprops=dict(
-        marker="o",
-        markerfacecolor="black",
-        markersize=1.0,
-        linestyle="none",
-        markeredgecolor="none",
-        alpha=0.6,
-    ),
-    ax=ax,
-)
-
-sns.stripplot(
-    "corr",
-    "cellline1",
-    data=plot_df,
-    orient="h",
-    edgecolor="white",
-    palette=PALETTE_PERTURB,
-    linewidth=0.1,
-    s=3,
-    ax=ax,
-)
-
-ax.grid(True, ls=":", lw=0.1, alpha=1.0, zorder=0)
-
-ax.set_xlabel("Replicated correlation\n(Pearson's R)")
-ax.set_ylabel("Condition")
-
-plt.savefig(f"{RPATH}/MultiOmics_perturb_repc_corr.pdf", bbox_inches="tight")
-plt.savefig(f"{RPATH}/MultiOmics_perturb_repc_corr.png", bbox_inches="tight", dpi=600)
-plt.close("all")
-
-#
-plot_df = prot_perturb.rename(columns=manifest["Cell Line"]).corr()
-col_colors = [PALETTE_PERTURB[c] for c in plot_df]
-row_colors = [PALETTE_PERTURB[c] for c in plot_df.index]
-
-g = sns.clustermap(
-    plot_df,
-    cmap="RdYlGn",
-    row_colors=row_colors,
-    col_colors=col_colors,
-    annot=True,
-    center=0,
-    fmt=".1f",
-    annot_kws=dict(size=4),
-    lw=0.05,
-    figsize=(8.5, 8.5),
-)
-plt.savefig(f"{RPATH}/MultiOmics_perturb_repc_corr_clustermap.pdf", bbox_inches="tight")
-plt.savefig(
-    f"{RPATH}/MultiOmics_perturb_repc_corr_clustermap.png", bbox_inches="tight", dpi=600
-)
-plt.close("all")
-
-#
-comparison = dict(
-    control=list(manifest[["10% FBS" in v for v in manifest["Cell Line"]]].index),
-    condition=list(manifest[["1% FBS" in v for v in manifest["Cell Line"]]].index),
-)
-
-diff_prot = pd.DataFrame(
-    ttest_ind(
-        prot_perturb[comparison["control"]].T,
-        prot_perturb[comparison["condition"]].T,
-        equal_var=False,
-        nan_policy="omit",
-    ),
-    index=["tstat", "pvalue"],
-    columns=prot_perturb.index,
-).T.astype(float).sort_values("pvalue").dropna()
-diff_prot["fdr"] = multipletests(diff_prot["pvalue"], method="fdr_bh")[1]
-diff_prot["diff"] = prot_perturb.loc[diff_prot.index, comparison["control"]].mean(1) - prot_perturb.loc[diff_prot.index, comparison["condition"]].mean(1)
-diff_prot["control_n"] = prot_perturb.loc[diff_prot.index, comparison["control"]].count(1)
-diff_prot["condition_n"] = prot_perturb.loc[diff_prot.index, comparison["condition"]].count(1)
-
-
-# ###
-genesets = ["c5.all.v7.1.symbols.gmt", "c2.all.v7.1.symbols.gmt"]
-
-dsets_dred = dict(diff_prot=diff_prot["diff"], mofa_factor=mofa.weights["proteomics"]["F2"])
-
-enr_pcs = pd.concat(
-    [
-        gseapy.ssgsea(
-            dsets_dred[ds],
-            processes=4,
-            gene_sets=Enrichment.read_gmt(f"{DPATH}/pathways/{g}"),
-            no_plot=True,
-        )
-        .res2d.assign(geneset=g)
-        .assign(dtype=ds)
-        .reset_index()
-        for ds in dsets_dred
-        for g in genesets
-    ],
-    ignore_index=True,
-)
-enr_pcs = enr_pcs.rename(columns={"sample1": "nes"}).sort_values("nes")
-
-# ####
-
-plot_df = pd.pivot_table(enr_pcs, index="Term|NES", columns="dtype", values="nes")
-
-f, ax = plt.subplots(1, 1, figsize=(2, 2), dpi=600)
-
-ax.scatter(
-    plot_df["diff_prot"], plot_df["mofa_factor"],
-    c=GIPlot.PAL_DBGD[2],
-    s=5,
-    linewidths=0,
-)
-
-gs_highlight = plot_df[
-    (plot_df[["diff_prot", "mofa_factor"]].abs() > .45).any(1)
-].sort_values("diff_prot").head(30)
-gs_highlight_dw = gs_highlight.query(f"diff_prot < 0").sort_values(
-    "diff_prot", ascending=False
-)
-gs_highlight_up = gs_highlight.query(f"diff_prot > 0").sort_values(
-    "diff_prot", ascending=True
-)
-gs_highlight_pal = pd.Series(
-    sns.light_palette(
-        "#3182bd", n_colors=len(gs_highlight_dw) + 1, reverse=True
-    ).as_hex()[:-1]
-    + sns.light_palette(
-        "#e6550d", n_colors=len(gs_highlight_up) + 1, reverse=False
-    ).as_hex()[1:],
-    index=gs_highlight.index,
-)
-
-for g in gs_highlight.index:
-    ax.scatter(
-        plot_df.loc[g, "diff_prot"],
-        plot_df.loc[g, "mofa_factor"],
-        c=gs_highlight_pal[g],
-        s=10,
-        linewidths=0,
-        label=g,
-    )
-
-cor, pval = spearmanr(plot_df["diff_prot"], plot_df["mofa_factor"], nan_policy="omit")
-annot_text = f"Spearman's R={cor:.2g}, p-value={pval:.1e}"
-ax.text(0.98, 0.02, annot_text, fontsize=4, transform=ax.transAxes, ha="right")
-
-ax.legend(
-    frameon=False, prop={"size": 4}, loc="center left", bbox_to_anchor=(1, 0.5)
-)
-
-ax.grid(True, ls=":", lw=0.1, alpha=1.0, zorder=0)
-
-ax.set_xlabel("FBS differential protein abundance\n(10% - 1%)")
-ax.set_ylabel("Factor 2")
-ax.set_title(f"PCs enrichment scores (NES)")
-
-plt.savefig(
-    f"{RPATH}/MultiOmics_perturb_gsea_corr.pdf",
-    bbox_inches="tight",
-)
-plt.savefig(
-    f"{RPATH}/MultiOmics_perturb_gsea_corr.png",
-    bbox_inches="tight",
-)
-plt.close("all")
